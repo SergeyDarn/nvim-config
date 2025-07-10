711e7e025a2caa46c173bb5682cdb59b8ec76383
Sergey Darn <sergeydarn@gmail.com>
2025-07-11 01:34:27 +0300

    Fix harpoon v2

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local harpoon = require("harpoon")
+
+vim.keymap.set("n", "<leader>hm", function()
+	harpoon:list():add()
+end, { desc = "Harpoon add mark" })
+
+vim.keymap.set("n", "<leader>hh", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Harpoon view marks" })
+
+vim.keymap.set("n", "<S-h>", function()
+	harpoon:list():prev({ ui_nav_wrap = true })
+end, { desc = "Harpoon prev mark" })
+
+vim.keymap.set("n", "<S-l>", function()
+	harpoon:list():next({ ui_nav_wrap = true })
+end, { desc = "Harpoon next mark" })
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,22 +7,6 @@ return {
 			local harpoon = require("harpoon")
 
 			harpoon:setup()
-
-			vim.keymap.set("n", "<leader>hm", function()
-				harpoon:list():add()
-			end, { desc = "Harpoon add mark" })
-
-			vim.keymap.set("n", "<leader>hh", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end, { desc = "Harpoon view marks" })
-
-			vim.keymap.set("n", "<S-H>", function()
-				harpoon:list():prev()
-			end, { desc = "Harpoon prev mark" })
-
-			vim.keymap.set("n", "<S-L>", function()
-				harpoon:list():next()
-			end, { desc = "Harpoon next mark" })
 		end,
 	},
 }
