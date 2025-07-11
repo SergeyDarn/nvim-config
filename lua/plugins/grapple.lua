933d2fb922429296a2ec932daab7c14d49bf12ae
Sergey Darn <sergeydarn@gmail.com>
2025-07-11 20:18:23 +0300

    fix: grapple commands description

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -8,8 +8,8 @@ return {
 			{ "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Toggle Mark" },
 			{ "<leader>h", "<cmd>Grapple toggle_tags<cr>", desc = "Show Marks" },
 
-			{ "<S-l>", "<cmd>Grapple cycle_tags next<cr>", desc = "Previous Mark" },
-			{ "<S-h>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Next Mark" },
+			{ "<S-l>", "<cmd>Grapple cycle_tags next<cr>", desc = "Next Mark" },
+			{ "<S-h>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Previous Mark" },
 		},
 		opts = {
 			win_opts = {
