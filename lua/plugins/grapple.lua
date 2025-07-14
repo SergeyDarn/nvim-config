79d8282c6b84ba38dcefb0ab813fee8716b8f7dc
Sergey Darn <sergeydarn@gmail.com>
2025-07-14 08:09:05 +0300

    feat: change mark shortcut, add notification message

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -5,7 +5,24 @@ return {
 			{ "nvim-tree/nvim-web-devicons", lazy = true },
 		},
 		keys = {
-			{ "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Toggle Mark" },
+			{
+				"<leader>a",
+				function()
+					local grapple = require("grapple")
+					local notifier = require("snacks.notifier")
+
+					local currentBuffer = vim.api.nvim_get_current_buf()
+					local mark = grapple.exists({ buffer = currentBuffer })
+					local message = "Mark added"
+					if mark then
+						message = "Mark deleted"
+					end
+
+					grapple.toggle()
+					notifier.notify(message, "info")
+				end,
+				desc = "Toggle Mark",
+			},
 			{ "<leader>h", "<cmd>Grapple toggle_tags<cr>", desc = "Show Marks" },
 
 			{ "<S-l>", "<cmd>Grapple cycle_tags next<cr>", desc = "Next Mark" },
